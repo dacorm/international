06a57e80423c20490196276fce5a6666509ee576
Utkin Denis <stolen.youth@yandex.ru>
2022-09-05 13:24:23 +0700

    feat: ready to deploy app

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4444/'
+    baseURL: 'https://mern-blog-dacorm.herokuapp.com/'
 });
 
 instance.interceptors.request.use((config) => {
