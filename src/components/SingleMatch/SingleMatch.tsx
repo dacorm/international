64e6f869858d94d8b0cd28f4fc240b725e750970
Utkin Denis <stolen.youth@yandex.ru>
2022-09-14 14:31:02 +0700

    feat: winners img

--- src/components/SingleMatch/SingleMatch.tsx
+++ src/components/SingleMatch/SingleMatch.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from "./SingleMatch.module.css";
+import cup from '../../assets/images/cup-svgrepo-com.svg';
 
 
 type SingleMatchProps = {
@@ -15,6 +16,8 @@ type SingleMatchProps = {
 
 const SingleMatch: React.FC<SingleMatchProps> = ({ leagueName, logo, logo2, playTime, direName, direScore, radiantName, radiantScore }) => {
 
+    const isWinner: boolean = Boolean(radiantScore > direScore)
+
     return (
         <div className={styles.singleMatch}>
             <h2 className={styles.listHeading}>{leagueName}</h2>
@@ -25,11 +28,13 @@ const SingleMatch: React.FC<SingleMatchProps> = ({ leagueName, logo, logo2, play
                     <div className={styles.team}>
                         <img src={logo} alt="Team1Logo" className={styles.logo}/>
                         <p className={styles.teamName}>{direName}</p>
+                        {!isWinner && (<img src={cup} alt="winner icon" className={styles.winner}/>)}
                         <p className={styles.score}>{direScore}</p>
                     </div>
                     <div className={styles.team}>
                         <img src={logo2} alt="Team2Logo" className={styles.logo}/>
                         <p className={styles.teamName}>{radiantName}</p>
+                        {isWinner && (<img src={cup} alt="winner icon" className={styles.winner}/>)}
                         <p className={styles.score}>{radiantScore}</p>
                     </div>
                 </div>
