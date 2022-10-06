b109f149cca0691a14b522fd395b8d2648d5f5eb
Utkin Denis <stolen.youth@yandex.ru>
2022-10-06 15:05:30 +0700

    fix: comments

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -44,24 +44,9 @@ export const commentsData = [{
 
 const Article = () => {
     const [comments, setComments] = useState([{
-        name: 'Elizabeth Valentine',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do\n' +
-            '                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud\n' +
-            '                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in\n' +
-            '                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-    },{
-        name: 'Thomas Stevens',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do\n' +
-            '                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud\n' +
-            '                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in\n' +
-            '                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-    },{
-        name: 'Megan Lebeu',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do\n' +
-            '                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud\n' +
-            '                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in\n' +
-            '                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-    }])
+        name: 'Denis',
+        text: 'Отличная новость, актуальная'
+    },])
     const [text, setText] = useState('');
     const [title, setTitle] = useState('');
     const [preview, setPreview] = useState('');
