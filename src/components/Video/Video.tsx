36712950269513e28aa38461c9234e530225cae3
Utkin Denis <stolen.youth@yandex.ru>
2022-10-26 16:11:51 +0700

    fix: slider fixes

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -17,18 +17,21 @@ const Video: React.FC<VideoProps> = ({src, title}) => {
 
 
     return (
-            visible ? <>
-                <iframe src={src ?? 'https://www.youtube.com/embed/qQzz2YLi0LI'}
-                        title={title ?? '[RU] OG vs Team Liquid – Game 2 - The International 2022 - Main Event Day 4'}
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen className={styles.iframeVideo}/>
-            </> : <div className={styles.video} onClick={handleClick}>
-                <div className={styles.overlay}>
-                    <img src={play} alt="Play" className={styles.play}/>
-                </div>
-                <p className={styles.text}>Последняя трансляция The Internatonal 2022</p>
-                <p className={styles.textSmall}>Нажми чтобы смотреть последние матчи play off в прямом эфире</p>
-            </div>
+        <div className={styles.video} onClick={handleClick}>
+            {visible ? <>
+                    <iframe src={src ?? 'https://www.youtube.com/embed/qQzz2YLi0LI'}
+                            title={title ?? '[RU] OG vs Team Liquid – Game 2 - The International 2022 - Main Event Day 4'}
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen className={styles.iframeVideo}/>
+                </> :
+                <>
+                    <div className={styles.overlay}>
+                        <img src={play} alt="Play" className={styles.play}/>
+                    </div>
+                    <p className={styles.text}>Последняя трансляция The Internatonal 2022</p>
+                    <p className={styles.textSmall}>Нажми чтобы смотреть последние матчи play off в прямом эфире</p>
+                </>}
+        </div>
     );
 };
 
