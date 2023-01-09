c8858a612c483084090353035c0156f05baf19e3
dacorm <stolen.youth@yandex.ru>
2023-01-09 12:02:16 +0700

    feat: sitemap generation

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { hydrate } from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
 
+const rootElement = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement,
 );
-root.render(
-    <BrowserRouter>
-        <React.StrictMode>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </React.StrictMode>
-    </BrowserRouter>,
-);
+
+if (rootElement.hasChildNodes()) {
+    hydrate(
+        <BrowserRouter>
+            <React.StrictMode>
+                <Provider store={store}>
+                    <App />
+                </Provider>
+            </React.StrictMode>
+        </BrowserRouter>,
+        rootElement,
+    );
+} else {
+    root.render(
+        <BrowserRouter>
+            <React.StrictMode>
+                <Provider store={store}>
+                    <App />
+                </Provider>
+            </React.StrictMode>
+        </BrowserRouter>,
+    );
+}
