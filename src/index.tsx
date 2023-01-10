96cb3d0246ef133301341ed474c3c1cda2cf0412
dacorm <stolen.youth@yandex.ru>
2023-01-10 12:03:22 +0700

    feat: back

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { hydrate } from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
 
-const rootElement = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement,
 );
 
-if (rootElement.hasChildNodes()) {
-    hydrate(
-        <BrowserRouter>
-            <React.StrictMode>
-                <Provider store={store}>
-                    <App />
-                </Provider>
-            </React.StrictMode>
-        </BrowserRouter>,
-        rootElement,
-    );
-} else {
-    root.render(
-        <BrowserRouter>
-            <React.StrictMode>
-                <Provider store={store}>
-                    <App />
-                </Provider>
-            </React.StrictMode>
-        </BrowserRouter>,
-    );
-}
+root.render(
+    <BrowserRouter>
+        <React.StrictMode>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </React.StrictMode>
+    </BrowserRouter>,
+);
