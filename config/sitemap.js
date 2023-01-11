2d401365d2ccbbd61f8afc9d1c7f5b6812955b72
dacorm <stolen.youth@yandex.ru>
2023-01-11 11:49:36 +0700

    feat: sitemap generation script

--- config/sitemap.js
+++ config/sitemap.js
@@ -1,48 +1,45 @@
-import path from 'path';
-import sm from 'sitemap';
-import fs from 'fs';
+const sm = require('sitemap');
+const path = require('path');
+const fs = require('fs');
 
-import config from '.';
-import data from '../data.json';
-import {
-    getAllPostsForListing,
-    getAllCategoriesForListing,
-} from '../selectors/data';
+const OUTPUT_FILE = path.resolve(__dirname, '..', 'public', 'sitemap.xml');
 
-const OUTPUT_FILE = path.resolve(__dirname, '..', '..', 'public', 'sitemap.xml');
-
-const postsUrls = getAllPostsForListing({ data })
-    .map((post) => {
-        const handle = [
-            post.handle.substring(0, 4),
-            post.handle.substring(5, 7),
-            post.handle.substring(8, 10),
-            post.handle.substring(11),
-        ].join('/');
-        return {
-            url: `${config.PUBLIC_URL}/${handle}`,
-            changefreq: 'weekly',
-            priority: 0.8,
-        };
-    });
-
-const categoriesUrls = getAllCategoriesForListing({ data })
-    .map((category) => ({
-        url: `${config.PUBLIC_URL}/category/${category.handle}`,
-        changefreq: 'weekly',
-        priority: 0.8,
-    }));
+// const postsUrls = getAllPostsForListing({ data })
+//     .map((post) => {
+//         const handle = [
+//             post.handle.substring(0, 4),
+//             post.handle.substring(5, 7),
+//             post.handle.substring(8, 10),
+//             post.handle.substring(11),
+//         ].join('/');
+//         return {
+//             url: `${config.PUBLIC_URL}/${handle}`,
+//             changefreq: 'weekly',
+//             priority: 0.8,
+//         };
+//     });
+//
+// const categoriesUrls = getAllCategoriesForListing({ data })
+//     .map((category) => ({
+//         url: `${config.PUBLIC_URL}/category/${category.handle}`,
+//         changefreq: 'weekly',
+//         priority: 0.8,
+//     }));
 
 const sitemap = sm.createSitemap({
-    hostname: 'https://bernardodiasdacruz.com',
-    cacheTime: 600000, // 600 sec (10 min) cache purge period
+    hostname: 'https://dota2.su/',
+    cacheTime: 600000,
     urls: [
         { url: '/', changefreq: 'weekly', priority: 1 },
-        { url: '/archive', changefreq: 'weekly', priority: 0.5 },
-        { url: '/search', changefreq: 'weekly', priority: 0.5 },
-        { url: '/about-me', changefreq: 'monthly', priority: 0.5 },
-        ...postsUrls,
-        ...categoriesUrls,
+        { url: '/login', changefreq: 'weekly', priority: 0.5 },
+        { url: '/register', changefreq: 'weekly', priority: 0.5 },
+        { url: '/profile', changefreq: 'monthly', priority: 0.5 },
+        { url: '/calendar', changefreq: 'monthly', priority: 0.5 },
+        { url: '/tournament', changefreq: 'monthly', priority: 0.5 },
+        { url: '/players', changefreq: 'monthly', priority: 0.5 },
+        { url: '/news', changefreq: 'monthly', priority: 0.5 },
+        { url: '/live', changefreq: 'monthly', priority: 0.5 },
+        { url: '/heroes', changefreq: 'monthly', priority: 0.5 },
     ],
 });
 
